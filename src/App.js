c34e7a93f9ab058e6e22c33c2cd10cd3981ebd01
DecoCrafttech <decocrafttech@gmail.com>
2025-01-17 13:30:12 +0530

    pending pages updated

--- src/App.js
+++ src/App.js
@@ -13,6 +13,24 @@ import Error from "Views/Common/error";
 import Layout from "Views/Main/Layout/Layout";
 import Home from "Views/Main/Pages/Home";
 
+import Contact from "Views/Main/Pages/Contact";
+import GlobalCapability from "Views/Main/Pages/GlobalCapability/GlobalCapability";
+import GlobalOverview from "Views/Main/Pages/GlobalCapability/GlobalOverview";
+import GlobalInitiation from "Views/Main/Pages/GlobalCapability/GlobalInitiation";
+import GlobalImplementation from "Views/Main/Pages/GlobalCapability/GlobalImplementation";
+import GlobalOngoing from "Views/Main/Pages/GlobalCapability/GlobalOngoing";
+import BuildOperator from "Views/Main/Pages/BuildOperator/BuildOperator";
+import Managedteams from "Views/Main/Pages/Managedteams/Managedteams";
+import ManagedOverview from "Views/Main/Pages/Managedteams/ManagedOverview";
+import ManagedRelationship from "Views/Main/Pages/Managedteams/ManagedRelationship";
+import ManagedRecord from "Views/Main/Pages/Managedteams/ManagedRecord";
+import ManagedServices from "Views/Main/Pages/Managedteams/ManagedServices";
+import BuildFeature from "Views/Main/Pages/BuildOperator/BuildFeature";
+import BuildInitiation from "Views/Main/Pages/BuildOperator/BuildInitiation";
+import Build from "Views/Main/Pages/BuildOperator/Build";
+import BuildOperate from "Views/Main/Pages/BuildOperator/BuildOperate";
+import Buildtransfer from "Views/Main/Pages/BuildOperator/Buildtransfer";
+
 
 const App = () => {
   const { token, user_id, isOnline, Err, Toast_Type } = useSelector((state) => state.commonState);
@@ -52,15 +70,38 @@ const App = () => {
     <HelmetProvider>
       <ToastContainer theme='light' />
       <Routes>
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
 
+    <Route path="/details" element={<GlobalCapability />}>
+    <Route index element={<GlobalOverview />} />
+    <Route path="initiation" element={<GlobalInitiation />} />
+    <Route path="implementation" element={<GlobalImplementation />} />
+    <Route path="ongoing-support" element={<GlobalOngoing />} />
+    </Route>
+    <Route path="/build-operator" element={<BuildOperator />}>
+    <Route index element={<BuildFeature />} />
+    <Route path="initiation" element={<BuildInitiation />} />
+    <Route path="build" element={<Build/>} />
+    <Route path="operate" element={<BuildOperate />} />
+    <Route path="transfer" element={<Buildtransfer />} />
+   
 
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
+    </Route>
+    <Route path="/managed-teams" element={<Managedteams />}>
+    <Route index element={<ManagedOverview/>} />
+      <Route path="relationship" element={<ManagedRelationship />} />
+      <Route path="record" element={<ManagedRecord />} />
+      <Route path="services" element={<ManagedServices />} />
 
-        {/* page not found  */}
-        <Route path="*" element={<Error />} />
-      </Routes>
+    </Route>
+    <Route path="/contact" element={<Contact />} />
+  </Route>
+
+  {/* page not found */}
+  <Route path="*" element={<Error />} />
+</Routes>
+
     </HelmetProvider >
     :
     <p>No internet connection</p>
